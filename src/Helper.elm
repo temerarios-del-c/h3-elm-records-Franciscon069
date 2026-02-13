477ab8f0d8a157c820837430e3affddf579c1e21
Franciscon069 <jvasillo@gamil.com>
2026-02-13 14:44:06 -0600

    Elm-test

--- src/Helper.elm
+++ src/Helper.elm
@@ -2,58 +2,36 @@ module Helper exposing (..)
 
 import Html exposing (Html, div, h1, ul, li, text)
 
+type alias Videogame = { title : String, releaseYear : Int, available : Bool, downloads : Int, genres : List String }
+type alias Computer = { ram : String, model : String, brand : String, screenSize : String }
 
--- registro videogame
-type alias Videogame =
-    { title : String
-    , releaseYear : Int
-    , available : Bool
-    , downloads : Int
-    , genres : List String
-    }
-
-
-type alias Computer =
-    { ram : String
-    , model : String
-    , brand : String
-    , screenSize : String
-    }
-
-
--- lista con al menos dos videojuegos
 videogames : List Videogame
 videogames =
-    [ { title = "Fornite"
-      , releaseYear = 2016
-      , available = True
-      , downloads = 1500000
-      , genres = [ "Action", "Shooter" ]
-      }
-    , { title = "Zelda"
-      , releaseYear = 2017
-      , available = True
-      , downloads = 7600000
-      , genres = [ "Action", "Adventure" ]
-      }
+    [ { title = "Fornite", releaseYear = 2016, available = True, downloads = 1500000, genres = [ "Action", "Shooter" ] }
+    , { title = "Zelda", releaseYear = 2017, available = True, downloads = 7600000, genres = [ "Action", "Adventure" ] }
     ]
 
-
--- variable myLaptop 
 myLaptop : Computer
-myLaptop =
-    { ram = "16GB"
-    , model = "MacBook Pro"
-    , brand = "Apple"
-    , screenSize = "14 inches"
-    }
+myLaptop = { ram = "16GB", model = "MacBook Pro", brand = "Apple", screenSize = "14 inches" }
 
+add2 : Int -> Int -> Int
+add2 x y = x + y
 
--- genero de los viedojuegos
+add3 : Float -> Float -> Float -> Float
+add3 x y z = x + y + z
+
+calc : Int -> Int -> (Int -> Int -> Int) -> Int
+calc x y op = op x y
+
+languageNames : List { name : String, releaseYear : Int, currentVersion : String } -> List String
+languageNames list = List.map .name list
+
+onlyStudents : List { name : String, uType : String } -> List String
+onlyStudents users =
+    List.map (\u -> if u.uType == "Student" then u.name else "") users
+
 getVideogameGenres : List Videogame -> List (List String)
-getVideogameGenres games =
-    List.map .genres games
-
+getVideogameGenres games = List.map .genres games
 
 main : Html msg
 main =
